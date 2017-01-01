e2ed36bf5919f36695784739bf9528885e72bce7
atnartur <i@atnartur.ru>
2017-01-01 15:34:25 +0300

    ! docker-compose-install.sh

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,4 +1,6 @@
-# @Author: Artur Atnagulov (ClienDDev team)
-# @Date:   2017-01-01 15:30:46
-# @Last Modified by:   atnartur
-# @Last Modified time: 2017-01-01 15:30:46
+# Comands from docker-compose official instruction
+# https://docs.docker.com/compose/install/
+apt-get install curl -y && 
+curl -L "https://github.com/docker/compose/releases/download/1.8.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose && 
+chmod +x /usr/local/bin/docker-compose && 
+docker-compose --version
