8caf209c8a05de4cfd6828fdf0139e1b4115848b
Artur <atnartur@users.noreply.github.com>
2017-02-21 00:05:07 +0300

    ! compose install script

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,6 +1,6 @@
 # Comands from docker-compose official instruction
 # https://docs.docker.com/compose/install/
 apt-get install curl -y && 
-curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose && 
+curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && 
 chmod +x /usr/local/bin/docker-compose && 
 docker-compose --version
