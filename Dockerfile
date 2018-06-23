bd07b25bc8be3bd7231b01d4266c147e419db3f2
atnartur <i@atnartur.ru>
2018-06-24 00:29:00 +0300

    = update versions

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM debian:latest
 ADD docker-install.sh .
 RUN ./docker-install.sh
 
-# From Docker`s image Dockerfile
-# https://github.com/docker-library/docker/blob/a7fc73eef011c47cc2518149bc77a4b9bc7f9f41/1.12/Dockerfile
 COPY docker-entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
--- docker-install.sh
+++ docker-install.sh
@@ -1,12 +1,21 @@
 # Comands from Docker official instruction
 # https://docs.docker.com/engine/installation/linux/debian/
 apt-get update && 
-apt-get install apt-transport-https ca-certificates gnupg2 -y && 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && 
-echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list && 
+apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common -y && 
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && 
+apt-key fingerprint 0EBFCD88 &&
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable" &&
+
 apt-get update && 
-apt-cache policy docker-engine && 
-apt-get install docker-engine -y &&
+apt-get install docker-ce -y &&
 docker -v &&
 
 echo "" && 
